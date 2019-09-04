4c664bb01232aa705c1b9868705ae73e16b02cdd
baconbits1204 <rafaelhermoso1204@gmail.com>
2019-09-03 23:37:35 -0400

    fixing issue with button clicks repeating values, commented out old code to fix later

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,81 @@ var loss = 0
 //functions ====================================
 
 var makeNum = function (min, max){
+    var random = Math.floor(Math.random() * (+max - +min)) + +min;
+    return random
+    }
+var numberTracker = function (addToNumber){
+    $('#current-number').text(addToNumber);
+}
+
+var compareNumbers = function (currentNumber, targetNumber) {
+    if (currentNumber === targetNumber) {
+        alert('You win!');
+        win++;
+        
+        $('.win').text(win);
+        start();
+    } else if (currentNumber > targetNumber) {
+        alert('You lose!')
+        loss++;
+
+        $('.loss').text(loss);
+        start();
+    }
+}
+
+
+var start = function (){
+targetNum = makeNum(19, 120);
+chrystal1 = makeNum(1, 12);
+chrystal2 = makeNum(1, 12);
+chrystal3 = makeNum(1, 12);
+chrystal4 = makeNum(1, 12);
+currentNum = 0;
+$('#current-number').text(currentNum);
+$('#target-num').text(targetNum);
+}
+
+start();
+
+
+$('#ruby-btn').click(function(){
+    currentNum = currentNum + chrystal1;
+    numberTracker(currentNum);
+    compareNumbers(currentNum, targetNum);
+ });
+$('#emerald-btn').click(function(){
+    currentNum = currentNum + chrystal2;
+    numberTracker(currentNum);
+    compareNumbers(currentNum, targetNum);
+});
+$('#sapphire-btn').click(function(){
+    currentNum = currentNum + chrystal3;
+    numberTracker(currentNum);
+    compareNumbers(currentNum, targetNum);
+});
+$('#amber-btn').click(function(){
+    currentNum = currentNum + chrystal4; 
+    numberTracker(currentNum);
+    compareNumbers(currentNum, targetNum);
+});
+
+/* 
+//Variables==================================
+
+var targetNum = 0
+var chrystal1 = 0 
+var chrystal2 = 0
+var chrystal3 = 0
+var chrystal4 = 0
+var currentNum = 0
+var win = 0
+var loss = 0
+
+
+//functions ====================================
+
+var makeNum = function (min, max){
     // var random = Math.random() * (+max - +min) + +min; 
     var random = Math.floor(Math.random() * (+max - +min)) + +min;
     return random
@@ -80,12 +155,5 @@ $('#sapphire-btn').click(function(){
 $('#amber-btn').click(function(){
     gameplay(currentNum, targetNum, chrystal4);
 });
-//write target number into its box
+*/
 
-//write current number into its box
-
-//if a chrystal is clicked add its value to current number
-
-//if current number === target number alert 'you win' and add 1 win to counter
-
-//else alert 'you lose' and add 1 loss
