145dd39e389986d1669512f2f39de3975147ccec
baconbits1204 <rafaelhermoso1204@gmail.com>
2019-09-03 23:23:04 -0400

    first working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,21 +13,73 @@ var loss = 0
 //functions ====================================
 
 var makeNum = function (min, max){
+    // var random = Math.random() * (+max - +min) + +min; 
     var random = Math.floor(Math.random() * (+max - +min)) + +min;
     return random
     }
+var numberTracker = function (addToNumber){
+    $('#current-number').text(addToNumber);
+}
+
+var counter = function (chosenNumber) {
+    currentNum = currentNum + chosenNumber;
+}
+
+var compareNumbers = function (currentNumber, targetNumber) {
+    if (currentNumber === targetNumber) {
+        alert('You win!');
+        win++;
+        
+        $('.win').text(win);
+        start();
+    } else if (currentNumber > targetNumber) {
+        alert('You lose!')
+        loss++;
+
+        $('.loss').text(loss);
+        start();
+    }
+}
+
+var gameplay = function(currentNumber, targetNumber, chosenNumber) {
+    counter(chosenNumber)
+    numberTracker(currentNumber);
+    compareNumbers(currentNumber, targetNumber);
+}
+
 
 var start = function (){
 targetNum = makeNum(19, 120);
 chrystal1 = makeNum(1, 12);
 chrystal2 = makeNum(1, 12);
 chrystal3 = makeNum(1, 12);
-chrystal4 = makeNum(1, 12);}
+chrystal4 = makeNum(1, 12);
+currentNum = 0;
+$('#current-number').text(currentNum);
+$('#target-num').text(targetNum);
+console.log(targetNum);
+console.log(chrystal1);
+console.log(chrystal2);
+console.log(chrystal3);
+console.log(chrystal4);
+console.log(currentNum);
+}
+
+start();
+
 //assign each chrystal to its button 
-$('#ruby-btn').click(function(){currentNum = currentNum + chrystal1});
-$('#emerald-btn').click(function(){currentNum = currentNum + chrystal1});
-$('#sapphire-btn').click(function(){currentNum = currentNum + chrystal1});
-$('#amber-btn').click(function(){currentNum = currentNum + chrystal1});
+$('#ruby-btn').click(function() {
+    gameplay(currentNum, targetNum, chrystal1);
+ });
+$('#emerald-btn').click(function(){
+    gameplay(currentNum, targetNum, chrystal2);
+});
+$('#sapphire-btn').click(function(){
+    gameplay(currentNum, targetNum, chrystal3);
+});
+$('#amber-btn').click(function(){
+    gameplay(currentNum, targetNum, chrystal4);
+});
 //write target number into its box
 
 //write current number into its box
@@ -37,32 +89,3 @@ $('#amber-btn').click(function(){currentNum = currentNum + chrystal1});
 //if current number === target number alert 'you win' and add 1 win to counter
 
 //else alert 'you lose' and add 1 loss
-
-
-// $("#ruby-number")
-// {
-
-// }
-
-// $("#emerald-number")
-// {
-
-// }
-
-// $("#sapphire-number")
-// {
-
-// }
-
-// $("#amber-number")
-// {
-
-// }
-console.log(makeNum)
-console.log(targetNum);
-console.log(chrystal1);
-console.log(chrystal2);
-console.log(chrystal3);
-console.log(chrystal4);
-console.log(currentNum);
-
